e3c3677d50cc4e4d9310c1446faf5515b9d24ac5
user 170 <C:\Users\user170\AppData\Roaming\The Bat!>
2024-05-23 14:28:32 +1000

    add two slider in about page

---
+++ assets/js/customSwiper.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
+
+const swiperCamp = new Swiper('.swiper-camp', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
