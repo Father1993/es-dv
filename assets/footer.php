c66033ebbe08eb4cfad517bf9024d28feccd5b24
user 170 <C:\Users\user170\AppData\Roaming\The Bat!>
2024-05-29 10:45:07 +1000

    fix bugs

--- assets/footer.php
+++ assets/footer.php
@@ -27,7 +27,6 @@
 <script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
 <script src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
 <script src="/assets/vendor/typed.js/typed.umd.js"></script>
-<script src="/assets/vendor/php-email-form/validate.js"></script>
 
 <!-- Template Main JS File -->
 <script src="/assets/js/main.js"></script>
