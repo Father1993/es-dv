292155beaae77000f67a8b731e5ef01433c4b1ba
user 170 <C:\Users\user170\AppData\Roaming\The Bat!>
2024-05-31 14:14:36 +1000

    add header-menu.php in main page

--- index.php
+++ index.php
@@ -38,100 +38,14 @@ require_once "./assets/header.php";
     <!-- ======= Header ======= -->
     <header id="header" class="d-flex align-items-center">
         <div class="container d-flex align-items-center justify-content-between">
-            <a href="/" class="logo mr-auto"><img src="assets/img/img1/elektrosistema-logo.png"
-                    alt="Логотип Электросистемы" /></a>
 
-            <nav id="navbar" class="navbar">
-                <ul>
-                    <li class="i-shop">
-                        <a class="nav-link scrollto" target="_blank" href="https://электротовары-дв.рф/">Интернет
-                            магазин</a>
-                    </li>
-                    <li>
-                        <a class="nav-link scrollto active" href="#hero">Главная</a>
-                    </li>
-                    <li>
-                        <a class="nav-link scrollto" href="#about">О нас</a>
-                    </li>
-                    <li>
-                        <a class="nav-link scrollto" href="#services">Услуги</a>
-                    </li>
-                    <li>
-                        <a class="nav-link scrollto" href="#portfolio">Продукция</a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#about"><span>Подробнее</span>
-                            <i class="bi bi-chevron-down"></i></a>
-                        <ul>
-                            <li>
-                                <a href="about.php">О компании</a>
-                            </li>
+            <!-- Header menu -->
+            <?php
+            // Require header_menu.php file for nav-menu
+            require_once "./assets/header_menu.php";
+            ?>
+            <!-- END Header menu -->
 
-                            <li class="dropdown">
-                                <a href="#services">
-                                    <span>Услуги</span>
-                                    <i class="bi bi-chevron-right"></i>
-                                </a>
-                                <ul>
-                                    <li>
-                                        <a href="services/electric-work.php">Все виды электромонтажных
-                                            работ</a>
-                                    </li>
-                                    <li>
-                                        <a href="services/laboratory.php">Услуги испытательной
-                                            лаборатории</a>
-                                    </li>
-                                    <li>
-                                        <a href="services/delivery.php">Доставка</a>
-                                    </li>
-                                    <li>
-                                        <a href="services/manufacturing.php">Изготовление металлических
-                                            корпусов электрощитов</a>
-                                    </li>
-
-                                    <li>
-                                        <a href="services/production.php">Производство электрощитов и
-                                            электрооборудования</a>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li class="dropdown">
-                                <a href="#portfolio"><span>Продукция</span>
-                                    <i class="bi bi-chevron-right"></i></a>
-                                <ul>
-                                    <li>
-                                        <a href="./products/svet/">Светотехника</a>
-                                    </li>
-                                    <li>
-                                        <a href="./products/cabel/">Кабель</a>
-                                    </li>
-                                    <li>
-                                        <a href="./products/silovoe/">Силовое и коммутационное
-                                            оборудование</a>
-                                    </li>
-                                    <li>
-                                        <a href="./products/elektromontazhnye-izdeliya/">Электромонтажные изделия</a>
-                                    </li>
-                                    <li>
-                                        <a href="./products/electro-ustanovka/">Электроустановочные изделия</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li><a href="vacancies.php">Вакансии</a></li>
-                            <li><a href="docs.php">Документы</a></li>
-                            <li>
-                                <a href="shop.php">Магазин Электромаркет</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a class="nav-link scrollto" href="contacts.php">Контакты</a>
-                    </li>
-                </ul>
-                <i class="bi bi-list mobile-nav-toggle"></i>
-            </nav>
-            <!-- .navbar -->
         </div>
     </header>
     <!-- End Header -->
