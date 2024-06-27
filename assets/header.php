6470f35ea79502ec848b67dc9f7c13f896d84f9e
user 170 <C:\Users\user170\AppData\Roaming\The Bat!>
2024-06-27 13:02:58 +1000

    fix bug metatag

--- assets/header.php
+++ assets/header.php
@@ -33,8 +33,10 @@
 
     <!-- Google Fonts -->
     <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800"
-        rel="stylesheet" />
+            href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i%7CRaleway:300,400,500,700,800"
+            rel="stylesheet"
+    />
+
 
     <!-- Vendor CSS Files -->
     <link href="/assets/vendor/aos/aos.css" rel="stylesheet" />
